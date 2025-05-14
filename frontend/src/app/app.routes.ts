4a3cc5a988c01952f1442d83b5a8e7d920308b18
Kevin Fries <kevinsctfries@gmail.com>
2025-05-13 20:50:17 -0400

    feat: add hero page with temp placeholders

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { NewSubjectComponent } from './new-subject/new-subject.component';
 import { SubjectConversationComponent } from './subject-conversation/subject-conversation.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'new-subject',
     component: NewSubjectComponent,
   },
   {
