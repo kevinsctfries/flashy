a43cf2a316ca5ba566faabeaf767aa58ec310959
Kevin Fries <kevinsctfries@gmail.com>
2025-05-13 21:37:34 -0400

    feat: add subject cards to hero page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ChatService, Subject } from '../services/chat.service';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,23 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  subjects: Subject[] = [];
+
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.loadSubjects();
+  }
+
+  private loadSubjects() {
+    this.chatService.getSubjects().subscribe({
+      next: (subjects) => {
+        this.subjects = subjects;
+      },
+      error: (error) => {
+        console.error('Error loading subjects:', error);
+      },
+    });
+  }
+}
